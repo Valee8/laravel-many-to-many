d711a168fbe47be57508fdab99dc3d168ed2f581
Vale <oirelav95@gmail.com>
2023-02-14 16:43:39 +0100

    pagina home che mostra i prodotti per categoria

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,22 @@
 @section('content')
 
     <h1>
-        Content
+        Categories
     </h1>
+
+    @foreach ($categories as $category)
+        <h2>{{ $category -> name }}</h2>
+        
+        <ul>
+            @foreach ($category -> products as $product)
+                <li>
+                    [{{ $product -> code }}] {{ $product -> name }}
+                    - {{ $product -> typology -> name }}
+                    - DIGITAL: 
+                    {{ $product -> typology -> digital ? "Yes" : "No" }}
+                </li>
+            @endforeach
+        </ul>
+    @endforeach
     
 @endsection
