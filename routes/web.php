3d1630a45060da5222d7b52799d65336e0f87165
Vale <oirelav95@gmail.com>
2023-02-15 01:21:33 +0100

    aggiungo rotte store e create per nuovo prodotto

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ use App\Http\Controllers\MainController;
 Route::get('/', [MainController::class, 'home']) -> name('home');
 
 Route::get('/product', [MainController::class, 'products']) -> name('product.home');
+
+Route::get('/product/create', [MainController :: class, 'productCreate']) -> name('product.create');
+
+Route::post('/product/store', [MainController :: class, 'productStore']) -> name('product.store');
